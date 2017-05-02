bc0aafe1b2ac1a143a5c2d31d1d0623594835791
GabrielAC17 <GabrielAC17@notebook>
2017-05-02 16:42:53 -0300

    Mudanças nos ícones

--- WindowsFormsApplication1/view/MainView.Designer.cs
+++ WindowsFormsApplication1/view/MainView.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.comboCars = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonNewCar = new System.Windows.Forms.Button();
@@ -362,6 +363,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboCars);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Sistema de Orçamentos Automotivos";
--- WindowsFormsApplication1/view/PartView.Designer.cs
+++ WindowsFormsApplication1/view/PartView.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PartView));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.nomeText = new System.Windows.Forms.TextBox();
@@ -162,6 +163,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PartView";
